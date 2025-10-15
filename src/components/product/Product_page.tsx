547815eb1f307d0e6cdb8fc2d1809398187a7977
Strelnikov Nikolay <113997874+strnikolay@users.noreply.github.com>
2025-10-15 15:48:12 +0300

    Update Product_page.tsx

--- src/components/product/Product_page.tsx
+++ src/components/product/Product_page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import "./Product_card.css";
 //import { Carousel } from "react-responsive-carousel";
 //import "react-responsive-carousel/lib/styles/carousel.min.css";
-import {brandList, categoryList, IProduct} from "@/api/db";
+import {brandList, categoryList} from "@/api/db";
 import {IProduct} from "@/store/interfaces";
 import { useStore } from "@/store/storeProvidert";
 import { observer } from 'mobx-react';
