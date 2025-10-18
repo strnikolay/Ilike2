7ba1bfe2d18e3f5b18759f7943b77e26f1d5c494
Strelnikov Nikolay <113997874+strnikolay@users.noreply.github.com>
2025-10-18 17:07:03 +0300

    Update select_transport.tsx

--- src/app/cart/select_transport.tsx
+++ src/app/cart/select_transport.tsx
@@ -1,1 +1,35 @@
-a
+'use client'
+import {FC, useEffect } from "react";
+//import { useStore } from "@/store/storeProvidert";
+import { observer } from 'mobx-react';
+
+
+interface Props {
+  type:string;
+  //itemIndex: number;
+}
+
+export const Select_Transport:FC<Props> = observer(({type}) => {
+  //const {Store, Cart_Store} = useStore();
+  //const [itemParams, setItemParams] = useState<IcartItemParam[]>([]);
+  
+ 
+  /*useEffect(()=>{
+    
+  },[])*/
+
+ 
+
+  return (
+     <label className={`btn `+type}>
+          <input
+            //onChange={()=>setSelectedReceipt("delivery")}
+            //checked={selectedReceipt==="delivery"}
+            name='delyvery-type'
+            type="radio"
+            className=""
+          />
+    </label>
+
+  )
+});
